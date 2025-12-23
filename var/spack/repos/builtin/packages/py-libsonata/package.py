6309fa10180f59a03fd47dbba2dc3075d50b967f
joseph-tharayil <tharayiljoe@gmail.com>
2025-12-23 17:31:59 +0100

    Highfive master dependency

--- var/spack/repos/builtin/packages/py-libsonata/package.py
+++ var/spack/repos/builtin/packages/py-libsonata/package.py
@@ -28,7 +28,7 @@ class PyLibsonata(PythonPackage):
     depends_on("cmake@3.16:", type="build")
     depends_on("fmt@7.1:")
     depends_on("hdf5@1.14:")
-    depends_on("highfive@2.9")
+    depends_on("highfive@master")
     depends_on("nlohmann-json@3.9.1")
     depends_on("py-pybind11@2.11.0:")
 
